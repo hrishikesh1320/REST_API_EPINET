89247c5292c4ca1ff2103d40beaac7281076775c
hrishikesh1320 <154308794+hrishikesh1320@users.noreply.github.com>
2024-01-02 12:33:33 +0530

    setting updated

--- companyapi/companyapi/settings.py
+++ companyapi/companyapi/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
